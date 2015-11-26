7c2ad0590b7ff4d325cccdebbfef79c181875161
muryoimpl <muryoimpl@gmail.com>
2015-11-27 00:15:37 +0900

    Add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+const postcss      = require('gulp-postcss');
+const gulp         = require('gulp');
+const sass         = require('gulp-sass');
+const sourcemaps   = require('gulp-sourcemaps');
+const concat       = require('gulp-concat');
+const autoprefixer = require('autoprefixer');
+const watch        = require('gulp-watch');
+const mqpacker     = require('css-mqpacker');
+const plumber      = require('gulp-plumber');
+const propsorter   = require('css-property-sorter');
+
+const paths = {
+  scss: './assets/css/**/*.scss',
+  css: './assets/css/'
+};
+
+gulp.task('scss:fmt', function() {
+  const processorsFmt = [
+    autoprefixer({ browsers: ['last 2 Chrome versions'] }),
+    mqpacker,
+    propsorter({ order: 'smaccs' })
+  ];
+
+  return gulp.src(paths.scss).
+    pipe(postcss(processorsFmt)).
+    pipe(gulp.dest(paths.css));
+});
+
+gulp.task('css', function() {
+  const processors = [
+    autoprefixer({ browsers: ['last 2 Chrome versions'] }),
+    mqpacker,
+    propsorter({ order: 'smaccs' })
+  ];
+
+  return gulp.src(paths.scss).
+    pipe(plumber()).
+    pipe(watch(paths.scss)).
+    pipe(sourcemaps.init()).
+    pipe(sass({ outputStyle: 'compressed' })).
+    on('error', function(err) {
+      console.log(err.message);
+    }).
+    pipe(postcss(processors)).
+    pipe(concat('main.css')).
+    pipe(sourcemaps.write()).
+    pipe(gulp.dest(paths.css));
+});
+
+gulp.task('default', ['css']);
