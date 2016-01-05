b708542b7bae43352bb5babc410a2c89fe819c29
muryoimpl <muryoimpl@gmail.com>
2016-01-06 02:21:33 +0900

    Add e2e test of Home component

---
+++ test/e2e/showHome.e2e.js
@@ -0,0 +1,10 @@
+/*global element, by*/
+const assert = require('power-assert');
+
+describe('show Home', () => {
+  it('should show "こんにちは"', () => {
+    element(by.className('title')).getText().then((title) => {
+      assert.ok(title, 'こんにちは');
+    });
+  });
+});
